3d446ef0e5b05e24732fab523a19b085a6401566
Sachin <sachinkshuklaoo7@gmail.com>
2026-03-21 13:51:16 +0530

    refactor: Remove content migration endpoints and related models from the API

--- src/api/routes/__init__.py
+++ src/api/routes/__init__.py
@@ -8,13 +8,11 @@ from src.api.routes.quiz import router as quiz_router
 from src.api.routes.interview_prep import router as interview_prep_router
 from src.api.routes.session import router as session_router
 from src.api.routes.aptitude import router as aptitude_router
-from src.api.routes.migration import router as migration_router
 
 __all__ = [
     "quiz_router",
     "interview_prep_router",
     "session_router",
     "aptitude_router",
-    "migration_router",
 ]
 
